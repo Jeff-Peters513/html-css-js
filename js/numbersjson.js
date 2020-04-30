c8d7271b8913a41eab80450e6f675fb74629ac69
Jeff Peters <petersjeff288@gmail.com>
2020-04-30 13:12:29 -0400

    updates to numbers work

---
+++ js/numbersjson.js
@@ -0,0 +1,32 @@
+let colors = ["red", "white", "blue"];
+let students = [
+    {name: "Andrew", favNumber: 27},
+    {name: "Jeff", favNumber: 5}, 
+    {name: "Trevor", favNumber: 11},
+    {name: "Tyler", favNumber: 24},
+    {name: "Dennis", favNumber: 6}
+];
+
+// declare function no parameters passed
+const numbers = () => {
+    for (i=1; i < 51; i++) {
+        let liTag = `<lu> ${i}`;
+        ulCtrl.innerHTML += liTag;
+    }
+
+    for (i=1; i < 51; i++) {
+          nbr =i;
+        if(nbr%3 === 0){
+            let three = `<lu> ${nbr}`;
+            let sum3 = nbr;
+            ulCtrl3.innerHTML += three;
+            ulCtrl3a.innerHTML = sum3;       
+        }
+        if(nbr % 5 === 0){
+            let five = `<lu> ${nbr}`;
+            ulCtrl5.innerHTML += five;
+            ulCtrl5a.innerHTML += nbr;        
+    }
+}
+}
+
