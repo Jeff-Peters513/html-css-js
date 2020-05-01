7f09f53a051c0d48e760fe7867adabc43024c4af
Jeff Peters <petersjeff288@gmail.com>
2020-05-01 11:06:11 -0400

    modified file

--- js/numbersjson.js
+++ js/numbersjson.js
@@ -8,25 +8,46 @@ let students = [
 ];
 
 // declare function no parameters passed
-const numbers = () => {
-    for (i=1; i < 51; i++) {
-        let liTag = `<lu> ${i}`;
-        ulCtrl.innerHTML += liTag;
-    }
+// const numbers = () => {
+//     for (i=1; i < 51; i++) {
+//         let liTag = `<lu> ${i}`;
+//         ulCtrl.innerHTML += liTag;
+//     }
 
-    for (i=1; i < 51; i++) {
-          nbr =i;
-        if(nbr%3 === 0){
-            let three = `<lu> ${nbr}`;
-            let sum3 = nbr;
-            ulCtrl3.innerHTML += three;
-            ulCtrl3a.innerHTML = sum3;       
+//     for (i=1; i < 51; i++) {
+//           nbr =i;
+//         if(nbr%3 === 0 || nbr % 5 === 0 ){
+//             let three = `<lu> ${nbr}`;
+//             let sum3 = nbr;
+//             ulCtrl3.innerHTML += three;
+//             ulCtrl3a.innerHTML = sum3;       
+//         }
+//         if(nbr % 5 === 0){
+//             let five = `<lu> ${nbr}`;
+//             ulCtrl5.innerHTML += five;
+//             ulCtrl5a.innerHTML += nbr;        
+//     }
+
+const numbers = () => {
+    for (let idx = 1; idx <= 50; i++) {
+
+        let nbr = idx;
+        if ((nbr % 3 == 0) && (nbr % 5 == 0)) {
+            // if number is a factor of 3 & 5 print out FizzBuzz
+            ulCtrl.innerHTML += liTag;
+            System.out.println("FizzBuzz");
+        } else if ((nbr % 5 == 0)) {
+            // else if number is a factor of 5 print out Buzz
+            System.out.println("Buzz");
+        } else if ((nbr % 3 == 0)) {
+            // else if number is a factor of 3 print out Fizz
+            document.getElementById("print") = nbr;
+            System.out.println("Fizz");
+        } else {
+            document.getElementById("print") = nbr;
         }
-        if(nbr % 5 === 0){
-            let five = `<lu> ${nbr}`;
-            ulCtrl5.innerHTML += five;
-            ulCtrl5a.innerHTML += nbr;        
     }
 }
-}
+
+
 
