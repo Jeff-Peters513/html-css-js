46158280ee63d4c21b4a07b6b534ca20dfed1e77
Jeff Peters <petersjeff288@gmail.com>
2020-05-04 16:32:54 -0400

    more updates to save

--- js/usercreate.js
+++ js/usercreate.js
@@ -1,22 +1,35 @@
+let url = "http://localhost:8080";
 
 $().ready (() => {
     //console.log("jQuery is ready")
     $("#save").click(()=>{
-        console.log("Inside click")
+        //console.log("Inside click")
         let newUser = {};
         newUser.id = Number($("#eid").val());
         newUser.userName = $("#eusername").val();
         newUser.password = $("#epassword").val();
         newUser.firstName = $("#efirstname").val();
         newUser.lastName = $("#elastname").val();
-        newUser.phonenumber = $("#ephonenumber").val();
+        newUser.phoneNumber = $("#ephonenumber").val();
         newUser.email = $("#eemail").val();
-        newUser.isReviewer = $("#eisreviewer").prop("checked");
-        newUser.isAdmin = $("#eisadmin").prop("checked");
+        newUser.reviewer = $("#eisreviewer").prop("checked");
+        newUser.admin = $("#eisadmin").prop("checked");
         console.log("User: ", newUser);
+        $.ajax({
+            method: "POST",
+            url: `${url}/users/`,
+            data: JSON.stringify(newUser),
+            contentType: "application/json"
 
-    })
-})
+        })
+            .done(() => {
+                console.log("User added!");
+            })
+            .fail((err) => {
+                console.error(err);
+            })
+    });
+});
 
 
 
